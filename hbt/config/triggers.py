65b1839e7980413050b08e6db93a48ef225ff47a
Ana Rita Alves Andrade <ana.alves.andrade@uni-hamburg.de>
2023-06-08 10:56:20 +0200

    WIP: added trigger information for 2018

--- hbt/config/triggers.py
+++ hbt/config/triggers.py
@@ -34,7 +34,7 @@ def add_triggers_2017(config: od.Config) -> None:
             tags={"single_trigger", "single_e", "channel_e_tau"},
         ),
         Trigger(
-            name="HLT_Ele32_WPTight_Gsf_L1DoubleEG",
+            name="HLT_Ele32_WPTight_Gsf_L1DoubnleEG",
             id=202,
             legs=[
                 TriggerLeg(
@@ -294,6 +294,242 @@ def add_triggers_2017(config: od.Config) -> None:
         # vbf
         #
         Trigger(
+            name="HLT_VBF_DoubleLooseChargedIsoPFTau20_Trk1_eta2p1_Reg",
+            id=601,
+            legs=[
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=25.0,
+                    # filter names:
+                    # hltDoublePFTau20TrackPt1LooseChargedIsolation
+                    trigger_bits=2048,
+                ),
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=25.0,
+                    # filter names:
+                    # hltDoublePFTau20TrackPt1LooseChargedIsolation
+                    trigger_bits=2048,
+                ),
+                # additional leg infos for vbf jets
+                TriggerLeg(
+                    min_pt=115.0,
+                    # filter names:
+                    # hltMatchedVBFOnePFJet2CrossCleanedFromDoubleLooseChargedIsoPFTau20
+                    trigger_bits=1,
+                ),
+                TriggerLeg(
+                    min_pt=40.0,
+                    # filter names:
+                    # hltMatchedVBFTwoPFJets2CrossCleanedFromDoubleLooseChargedIsoPFTau20
+                    trigger_bits=1,
+                ),
+            ],
+            applies_to_dataset=(lambda dataset_inst: dataset_inst.is_mc or dataset_inst.x.era >= "D"),
+            tags={"cross_trigger", "cross_tau_tau_vbf", "channel_tau_tau"},
+        ),
+    ])
+ 
+def add_triggers_2018(config:od.Config) -> None:
+    config.x.triggers = od.UniqueObjectIndex(Trigger, [
+    #
+    # single electron
+    #
+    Trigger(
+            name="HLT_Ele32_WPTight_Gsf",
+            id=201,
+            legs=[
+                TriggerLeg(
+                    pdg_id=11,
+                    min_pt=35.0,
+                    # filter names:
+                    # hltEle32WPTightGsfTrackIsoFilter
+                    trigger_bits=2,
+                ),
+            ],
+            applies_to_dataset=(lambda dataset_inst: dataset_inst.is_mc or dataset_inst.x.era >= "D"),
+            tags={"single_trigger", "single_e", "channel_e_tau"},
+        ),
+    Trigger(
+            name="HLT_Ele35_WPTight_Gsf",
+            id=203,
+            legs=[
+                TriggerLeg(
+                    pdg_id=11,
+                    min_pt=38.0,
+                    # filter names:
+                    # hltEle35noerWPTightGsfTrackIsoFilter
+                    trigger_bits=2,
+                ),
+            ],
+            tags={"single_trigger", "single_e", "channel_e_tau"},
+        ),
+    
+    #
+    # single muon
+    #
+    Trigger(
+            name="HLT_IsoMu24",
+            id=101,
+            legs=[
+                TriggerLeg(
+                    pdg_id=13,
+                    min_pt=26.0,
+                    # filter names:
+                    # hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07
+                    trigger_bits=2,
+                ),
+            ],
+            tags={"single_trigger", "single_mu", "channel_mu_tau"},
+        ),
+    Trigger(
+            name="HLT_IsoMu27",
+            id=102,
+            legs=[
+                TriggerLeg(
+                    pdg_id=13,
+                    min_pt=29.0,
+                    # filter names:
+                    # hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07
+                    trigger_bits=2,
+                ),
+            ],
+            tags={"single_trigger", "single_mu", "channel_mu_tau"},
+        ),
+
+    #
+    # e tauh
+    #
+    Trigger(
+            name="HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1",
+            id=401,
+            legs=[
+                TriggerLeg(
+                    pdg_id=11,
+                    min_pt=27.0,
+                    # filter names:
+                    # hltEle24erWPTightGsfTrackIsoFilterForTau
+                    # hltOverlapFilterIsoEle24WPTightGsfLooseIsoPFTau30
+                    trigger_bits=2 + 64,
+                ),
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=35.0,
+                    # filter names:
+                    # hltSelectedPFTau30LooseChargedIsolationL1HLTMatched
+                    # hltOverlapFilterIsoEle24WPTightGsfLooseIsoPFTau30
+                    trigger_bits=1024 + 256,
+                ),
+            ],
+            # the non-HPS path existed only for data and is fully covered in MC below
+            applies_to_dataset=(lambda dataset_inst: dataset_inst.is_data),
+            tags={"cross_trigger", "cross_e_tau", "channel_e_tau"},
+        ),
+
+    #
+    # mu tauh
+    #
+    Trigger(
+            name="HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1",
+            id=301,
+            legs=[
+                TriggerLeg(
+                    pdg_id=13,
+                    min_pt=22.0,
+                    # filter names:
+                    # hltL3crIsoL1sMu18erTau24erIorMu20erTau24erL1f0L2f10QL3f20QL3trkIsoFiltered0p07
+                    # hltOverlapFilterIsoMu20LooseChargedIsoPFTau27L1Seeded
+                    trigger_bits=2 + 64,
+                ),
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=32.0,
+                    # filter names:
+                    # hltSelectedPFTau27LooseChargedIsolationAgainstMuonL1HLTMatched or
+                    # hltOverlapFilterIsoMu20LooseChargedIsoPFTau27L1Seeded
+                    trigger_bits=1024 + 512,
+                ),
+            ],
+            # the non-HPS path existed only for data and is fully covered in MC below
+            applies_to_dataset=(lambda dataset_inst: dataset_inst.is_data),
+            tags={"cross_trigger", "cross_mu_tau", "channel_mu_tau"},
+        ),
+
+
+    #
+    # tauh tauh
+    #
+    Trigger(
+            name="HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg",
+            id=502,
+            legs=[
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=40.0,
+                    # filter names:
+                    # hltDoublePFTau35TrackPt1TightChargedIsolationAndTightOOSCPhotonsDz02Reg
+                    trigger_bits=64,
+                ),
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=40.0,
+                    # filter names:
+                    # hltDoublePFTau35TrackPt1TightChargedIsolationAndTightOOSCPhotonsDz02Reg
+                    trigger_bits=64,
+                ),
+            ],
+            applies_to_dataset=(lambda dataset_inst: dataset_inst.is_data),
+            tags={"cross_trigger", "cross_tau_tau", "channel_tau_tau"},
+        ),
+    Trigger(
+            name="HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg",
+            id=503,
+            legs=[
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=45.0,
+                    # filter names:
+                    # hltDoublePFTau40TrackPt1MediumChargedIsolationAndTightOOSCPhotonsDz02Reg
+                    trigger_bits=64,
+                ),
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=45.0,
+                    # filter names:
+                    # hltDoublePFTau40TrackPt1MediumChargedIsolationAndTightOOSCPhotonsDz02Reg
+                    trigger_bits=64,
+                ),
+            ],
+            applies_to_dataset=(lambda dataset_inst: dataset_inst.is_data),
+            tags={"cross_trigger", "cross_tau_tau", "channel_tau_tau"},
+        ),
+    Trigger(
+            name="HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg",
+            id=504,
+            legs=[
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=45.0,
+                    # filter names:
+                    # hltDoublePFTau40TrackPt1TightChargedIsolationDz02Reg
+                    trigger_bits=64,
+                ),
+                TriggerLeg(
+                    pdg_id=15,
+                    min_pt=45.0,
+                    # filter names:
+                    # hltDoublePFTau40TrackPt1TightChargedIsolationDz02Reg
+                    trigger_bits=64,
+                ),
+            ],
+            applies_to_dataset=(lambda dataset_inst: dataset_inst.is_data),
+            tags={"cross_trigger", "cross_tau_tau", "channel_tau_tau"},
+        ),
+   
+    #
+    # vbf
+    #
+    Trigger(
             name="HLT_VBF_DoubleLooseChargedIsoPFTau20_Trk1_eta2p1_Reg",
             id=601,
             legs=[
