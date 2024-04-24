7a6736344b57b8650dd10405a40f41250c2a1ac2
Nathan Prouvost <nathan.prouvost@gmail.com>
2024-04-24 13:35:47 +0200

    add IFNanoV12 option and apply it to lepton selection

--- hbt/selection/lepton.py
+++ hbt/selection/lepton.py
@@ -10,7 +10,7 @@ from columnflow.selection import Selector, SelectionResult, selector
 from columnflow.columnar_util import set_ak_column
 from columnflow.util import DotDict, maybe_import
 
-from hbt.util import IF_NANO_V9, IF_NANO_V11
+from hbt.util import IF_NANO_V9, IF_NANO_V11, IF_NANO_V12
 from hbt.config.util import Trigger
 
 
@@ -45,6 +45,7 @@ def trigger_object_matching(
         "Electron.pfRelIso03_all",
         IF_NANO_V9("Electron.mvaFall17V2Iso_WP80", "Electron.mvaFall17V2Iso_WP90", "Electron.mvaFall17V2noIso_WP90"),
         IF_NANO_V11("Electron.mvaIso_WP80", "Electron.mvaIso_WP90", "Electron.mvaNoIso_WP90"),
+        IF_NANO_V12("Electron.mvaIso_WP80", "Electron.mvaIso_WP90", "Electron.mvaNoIso_WP90"),
         "TrigObj.pt", "TrigObj.eta", "TrigObj.phi",
     },
     exposed=False,
