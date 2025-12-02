e18c795d106d0c7e8a36ba1b351eb7665be73404
Nathan Prouvost <nathan.prouvost@gmail.com>
2025-12-02 14:07:40 +0100

    modify datacard to get better variability in examples

--- hbt/studies/datacards/qcd_test.py
+++ hbt/studies/datacards/qcd_test.py
@@ -4,7 +4,7 @@ import os
 
 import hist
 
-from columnflow.inference import InferenceModel, ParameterType, FlowStrategy
+from columnflow.inference import InferenceModel, ParameterType, FlowStrategy  # , ParameterTransformations
 from columnflow.inference.cms.datacard import DatacardWriter
 
 
@@ -19,12 +19,17 @@ class QCDModel(InferenceModel):
             flow_strategy=FlowStrategy.warn,  # warn if under/overflow bins have non-zero content
         )
 
-        self.add_process(name="hh", is_signal=True)
+        self.add_process(name="ggHH_kl_1_kt_1_13p6TeV_hbbhtt", is_signal=True)
+        self.add_process(name="ggHH_kl_2p45_kt_1_13p6TeV_hbbhtt", is_signal=True)
+        self.add_process(name="ggHH_kl_5_kt_1_13p6TeV_hbbhtt", is_signal=True)
         self.add_process(name="tt", is_signal=False)
         self.add_process(name="qcd", is_signal=False)
+        self.add_process(name="full_qcd", is_signal=False)
 
-        self.add_parameter("BR_hbb", type=ParameterType.rate_gauss, process="hh", effect=(0.9874, 1.0124))
+        self.add_parameter("BR_hbb", type=ParameterType.rate_gauss, process=["*_13p6TeV_hbbhtt"], effect=(0.9874, 1.0124))  # noqa: E501
         self.add_parameter("pdf_gg", type=ParameterType.rate_gauss, process="tt", effect=1.042)
+        # self.add_parameter("scale_gg", type=ParameterType.shape, process="ggHH_kl_1_kt_1_13p6TeV_hbbhtt", effect=1.06, transformations=ParameterTransformations(["effect_from_rate"]))  # noqa: E501
+        self.add_parameter("scale_gg", type=ParameterType.shape, process="ggHH_kl_1_kt_1_13p6TeV_hbbhtt")  # noqa: E501
 
 
 def create_hist(values, variances, flow=False):
@@ -38,16 +43,22 @@ def create_hist(values, variances, flow=False):
 
 # dummy histograms, 2 bins
 h_hh = create_hist([0.5, 0.5], [0.1, 0.1])
+h_hh_up = create_hist([0.51, 0.55], [0.1, 0.1])
+h_hh_down = create_hist([0.45, 0.49], [0.1, 0.1])
 h_tt = create_hist([10.0, 0.25], [1.0, 0.1])
 h_qcd = create_hist([5.1, 0.0], [0.5, 0.0])
+h_full_qcd = create_hist([5.1, 0.25], [0.5, 0.0])
 
 # histogram structure expected by the datacard writer
 # category -> process -> config -> shift -> hist
 datacard_hists = {
     "single_category": {
-        "hh": {"22pre_v14": {"nominal": h_hh}},
+        "ggHH_kl_1_kt_1_13p6TeV_hbbhtt": {"22pre_v14": {"nominal": h_hh, ("scale_gg", "up"): h_hh_up, ("scale_gg", "down"): h_hh_down}},  # noqa: E501
+        "ggHH_kl_2p45_kt_1_13p6TeV_hbbhtt": {"22pre_v14": {"nominal": h_hh}},
+        "ggHH_kl_5_kt_1_13p6TeV_hbbhtt": {"22pre_v14": {"nominal": h_hh}},
         "tt": {"22pre_v14": {"nominal": h_tt}},
         "qcd": {"22pre_v14": {"nominal": h_qcd}},
+        "full_qcd": {"22pre_v14": {"nominal": h_full_qcd}},
     },
 }
 
@@ -55,6 +66,7 @@ datacard_hists = {
 qcd_model = QCDModel()
 writer = DatacardWriter(qcd_model, datacard_hists)
 this_dir = os.path.dirname(os.path.abspath(__file__))
+print(f"Writing datacard to {this_dir}/datacard.txt")
 writer.write(
     os.path.join(this_dir, "datacard.txt"),
     os.path.join(this_dir, "shapes.root"),
